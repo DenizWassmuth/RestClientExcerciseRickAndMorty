ef51b1be2b9e166006c4ca61a97d74f9a7f9881d
DenizWassmuth <deniz-wassmuth@gmx.de>
2026-01-14 11:33:29 +0100

    initial commit

---
+++ src/main/java/org/example/restclientexcerciserickandmorty/RestClientExcerciseRickAndMortyApplication.java
@@ -0,0 +1,13 @@
+package org.example.restclientexcerciserickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientExcerciseRickAndMortyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientExcerciseRickAndMortyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restclientexcerciserickandmorty/RestClientExcerciseRickAndMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restclientexcerciserickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientExcerciseRickAndMortyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
